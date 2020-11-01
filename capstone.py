a55f2ad66970a0071090fb5ef2cc243f25a6a2d8
viktar_b <viktar.beliakou@gmail.com>
2020-11-01 14:05:53 +0000

    changes

--- capstone.py
+++ capstone.py
@@ -1,9 +1,29 @@
-import folium
+import folium 
 
-def show_toronto_area(): 
-    top_right = (43.857698, -79.169111)
-    top_left = (43.750724, -79.640452)
-    bottom_left = (43.577857, -79.541984)
-    bottom_right = (43.749704, -79.111260)
-    toronto_map = folium.Map(location=[43.7, -79.4], zoom_start=11)
 
+def show_toronto_map(topright, topleft, bottomleft, bottomrigth):
+
+    boundaries = [  
+        topright, 
+        topleft,
+        bottomleft,
+        bottomrigth,
+        topright
+    ]
+
+    toronto_map = folium.Map(location=[43.7, -79.4], zoom_start=10)
+    toronto_map.choropleth(
+        geo_data='Toronto2.geojson',
+        fill_color='Yellow', 
+        line_color='Red',
+        fill_opacity=0.9, 
+        line_opacity=0.1,
+        reset=True
+    )
+
+    folium.PolyLine(boundaries, color="red", weight=2.5, opacity=1).add_to(toronto_map)
+
+    return toronto_map
+
+def print_this():
+    return 'qwe'
